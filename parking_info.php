f30aa3f1ecaa1bdbe8eda4d4475fcfa17a4945cd
May Lin <ongml95@gmail.com>
2020-10-24 17:18:33 +0800

    parking info interface

---
+++ parking_info.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<style>
+h1.a{font-family: "Comic Sans MS", cursive, sans-serif;}
+
+</style>
+<body>
+<h1 class="a"> Welcome to Smart Parking System </h1>
+<label for="cars">Choose a location:</label>
+  <select name="location" id="location">
+    <option >Setapak Mall</option>
+    <option >Pavillion</option>
+    <option >One Utama Mall</option> 
+  </select> <br>
+<label for="p_space">Available Parking Space :</label ><br>
+<label for="p_rate">Parking Rate :</label ><br>
+
+</head>
+
+</body>
+
+</html>
