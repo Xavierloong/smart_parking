a79b3fa6e8f56d36e2802c78c58a42128c12f4ad
intacten <67138775+intacten@users.noreply.github.com>
2020-11-08 23:16:56 +0800

    Add files via upload

---
+++ SPS/javascript/SPS.js
@@ -0,0 +1,9 @@
+var login = document.getElementById("login");
+var CreateAcc = document.getElementById("CreateAcc");
+var toggle = document.getElementById("btn");
+
+function register(){
+    login.style.left = "-400px";
+    CreateAcc.style.left = "50px";
+    toggle.style.left = "110px";
+}
